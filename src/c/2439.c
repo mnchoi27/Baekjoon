a649add867bdc339d83377e9d7ad484e041b091a
mnchoi27 <mnchoi27@gmail.com>
2023-08-05 01:00:41 +0900

    Add 2439.c

---
+++ src/c/2439.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 1; i <= N; i++) { // detect layer
+        for(int j = 1; j <= (N - i); j++) // reverse
+            printf(" ");
+        for(int j = 1; j <= i; j++) // repeat
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
