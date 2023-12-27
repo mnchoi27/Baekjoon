bb5ab6f0540095b62825e86aafe295f17795a051
mnchoi27 <mnchoi27@gmail.com>
2023-12-27 21:15:29 +0900

    Update 25206.c

--- src/c/25206.c
+++ src/c/25206.c
@@ -3,31 +3,26 @@
 int main(void) {
     char subject_name[50], grade[3];
     double total_grade = 0, total_credit = 0;
-    double credit;
+    double credit, grade_value;
 
     for(int i = 0; i < 20; i++) {
         scanf("%s %lf %s", subject_name, &credit, grade);
         // cal total grade from grade
-        if(grade[0] == 'A' && grade[1] == '+')
-            total_grade += credit * 4.5;
-        else if(grade[0] == 'A' && grade[1] == '0')
-            total_grade += credit * 4.0;
-        else if(grade[0] == 'B' && grade[1] == '+')
-            total_grade += credit * 3.5;
-        else if(grade[0] == 'B' && grade[1] == '0')
-            total_grade += credit * 3.0;
-        else if(grade[0] == 'C' && grade[1] == '+')
-            total_grade += credit * 2.5;
-        else if(grade[0] == 'C' && grade[1] == '0')
-            total_grade += credit * 2.0;
-        else if(grade[0] == 'D' && grade[1] == '+')
-            total_grade += credit * 1.5;
-        else if(grade[0] == 'D' && grade[1] == '0')
-            total_grade += credit * 1.0;
-        // except for P grade in total credit
+        switch(grade[0]) {
+            case 'A': grade_value = 4.0; break;
+            case 'B': grade_value = 3.0; break;
+            case 'C': grade_value = 2.0; break;
+            case 'D': grade_value = 1.0; break;
+            case 'F': grade_value = 0.0; break;
+            // except for P grade in total credit
+            default: continue;
+        }
+        // add 0.5 point for grades with +
+        if(grade[1] == '+')
+            grade_value += 0.5;
+        // update total grade and total credit
+        total_grade += credit * grade_value;
         total_credit += credit;
-        if(grade[0] == 'P')
-            total_credit -= credit;
     }
     printf("%lf", total_grade / total_credit);
 
